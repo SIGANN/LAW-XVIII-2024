97543a51e11d522e32c335ab8694726a01df157c
Sophie Henning <sophie.e.henning@gmail.com>
2024-02-03 18:20:44 +0100

    Add info on ELRA sponsorship

--- build.py
+++ build.py
@@ -53,7 +53,7 @@ import shutil
 # these as needed.
 
 PAGES = ["cfp", "index", "committee"]
-IMAGES = [os.path.join("images", "logo", "LAW18_Logo.svg")]
+IMAGES = [os.path.join("images", "logo", "LAW18_Logo.svg"), os.path.join("images", "ELRA_logo.svg")]
 STYLESHEETS = [os.path.join("css", "law2023.css")]
 
 
